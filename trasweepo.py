ac2cd551e1e40f56076e1192ff0619bc8fab3659
ldconejo <luisconej@gmail.com>
2014-10-20 12:55:05 -0700

    Procedure definitions
    Added procedure definitions. No real code yet.

--- trasweepo.py
+++ trasweepo.py
@@ -3,7 +3,8 @@ __author__ = 'luisconejo'
 
 # Individual procedures
 
-# Creates the database for results
+# Creates the database for results. The database will have a single table with the following fields
+# Month, Day, Hour, Minute (rounded to the nearest 5-minute block), origin, destination
 def createDB():
     pass
 
@@ -17,8 +18,17 @@ def saveIntoDB(startingAddress, destinationAddress, totalTime):
     pass
 
 # Calls getDirections for a particular route, process the output to extract the total driving time and call saveIntoDB
-# to save the results to the database
-def collectData():
+# to save the results to the database, adjusts the time stamp using roundTimeStamp.
+def collectData(startingAddress, destinationAddress):
     pass
 
-# f
+# Rounds a time stamp to match one particular day-of-the-year-hour-minute combination. These will be space five minutes
+# from each other. There are 288 readings per day with a maximum of 366 days per year, that gives a fixed database size
+# of 105408, doubled to 210816 entries, as time in both directions will be calculated.
+def roundTimeStamp():
+    pass
+
+# Runs indefinitely until a key is pressed, collecting data every five (5) minutes
+# Uses a time and collectData
+def infiniteRunner(startingAddress, destinationAddress):
+    pass
