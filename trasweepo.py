2bb5c15b7299d090379705acab446671630f00be
ldconejo <luisconej@gmail.com>
2014-10-22 00:15:43 -0700

    Version 1.0
    Added feature to run for an entire year.
    Enabled database.
    This is the first version of the finished script.
    Hard coded to calculate the distance from a place in Foster City to
    Intel Corporation in Santa Clara, CA.

--- trasweepo.py
+++ trasweepo.py
@@ -5,13 +5,24 @@ import urllib2
 import json
 import re
 import datetime
+import sqlite3
+import time
 
 # Individual procedures
 
 # Creates the database for results. The database will have a single table with the following fields
 # Month, Day, Hour, Minute (rounded to the nearest 5-minute block), origin, destination
-def createDB():
-    pass
+def initDB():
+    #Start database
+    global conn
+    conn = sqlite3.connect('trafficData.db')
+    global c
+    c = conn.cursor()
+    try:
+        c.execute('''CREATE TABLE trafficResults
+             (month, day, hour, minute, origin, destination, distance, duration)''')
+    except:
+        pass
 
 # Sends a request to Google Directions API, taking a starting address, destination address and returning the JSON
 # output
@@ -22,9 +33,31 @@ def getDirections(startingAddress, destinationAddress, apiKey):
     response = urllib2.urlopen(apiCommand)
     return response
 
-# Saves a result into the database
-def saveIntoDB(startingAddress, destinationAddress, totalTime):
-    pass
+# Saves a result into the database or updates and existing result
+def saveIntoDB(dBmonth, dBday, dBhour, dBminute, dBorigin, dBdestination, dBdistance, dBduration):
+    # First, check if there is an existing record with the same date and time
+    c.execute('SELECT * FROM trafficResults WHERE month=? AND day=? AND hour=? AND minute=? AND origin=?'
+              'AND destination = ? AND distance=?',
+              (dBmonth, dBday, dBhour, dBminute, dBorigin, dBdestination, dBdistance))
+
+    result = c.fetchone()
+
+    #If word is unknown, then do a web search
+    if str(result) == "None":
+        # Create a new record
+        result = c.execute("""INSERT INTO trafficResults(month, day, hour, minute, origin, destination,
+        distance, duration)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)""", (dBmonth, dBday, dBhour, dBminute, dBorigin, dBdestination,
+                                             dBdistance, dBduration))
+        conn.commit()
+        print "INFO: Record added"
+    else:
+        # Update existing record
+        c.execute('UPDATE trafficResults SET duration=? WHERE month=? AND day=? AND hour=? AND minute=? AND origin=?'
+                  'AND destination=? AND distance=?',
+                  (dBduration, dBmonth, dBday, dBhour, dBminute, dBorigin, dBdestination, dBdistance))
+        conn.commit()
+        print "INFO: Record updated"
 
 # Calls getDirections for a particular route, process the output to extract the total driving time and call saveIntoDB
 # to save the results to the database, adjusts the time stamp using roundTimeStamp.
@@ -50,21 +83,22 @@ def collectData(startingAddress, destinationAddress, serverKey):
     roundedCurrentMinute = 5 * (currentTime.minute / 5)
 
     #Save to database
-    print "Database entry:"
-    print currentTime.year
-    print currentTime.month
-    print currentTime.day
-    print currentTime.hour
-    print roundedCurrentMinute
-    print startingAddress
-    print destinationAddress
-    print distance[0]
-    print duration[0]
+    print str(currentTime.month) + "     " + str(currentTime.day) + "     " + str(currentTime.hour) + "     " + \
+          str(roundedCurrentMinute) + "       " + str(startingAddress)  + "  " + str(destinationAddress)  + "     " + \
+          str(distance[0]) + "     " + str(duration[0])
+
+    saveIntoDB(currentTime.month, currentTime.day, currentTime.hour, roundedCurrentMinute, startingAddress,
+               destinationAddress, distance[0], duration[0])
 
 # Runs indefinitely until a key is pressed, collecting data every five (5) minutes
 # Uses a time and collectData
-def infiniteRunner(startingAddress, destinationAddress):
-    pass
+def infiniteRunner(startingAddress, destinationAddress, serverKey):
+    counter = 0
+    while counter < 210816:
+        collectData(homeAddress,workAddress, serverKey)
+        collectData(workAddress,homeAddress, serverKey)
+        time.sleep(300)
+        counter = counter + 1
 
 # Main Flow
 homeAddress = "72+Rock+Harbor+Lane,+Foster+City,+CA+94404"
@@ -73,4 +107,11 @@ workAddress = "3600+Mission+College+Blvd,++Santa+Clara,+CA+95054"
 # Directions API server key
 serverKey = "AIzaSyC6oQWg74G15hWw5bab-EVX2BZNKz-cN3w"
 
-collectData(homeAddress,workAddress, serverKey)
+# Start DB
+initDB()
+
+# Print header for terminal display
+print "MONTH  DAY    HOUR   MINUTE  ORIGIN                                      DESTINATION                 " \
+      "                          DISTANCE DURATION"
+
+infiniteRunner(homeAddress,workAddress, serverKey)
